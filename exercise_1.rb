4bf4ce7287536ec2ba92eed1f2e738a34af91916
Richard Hart <richard@ur-ban.com>
2009-11-24 18:25:19 +0000

    Added exercise 1 file.

---
+++ exercise_1.rb
@@ -0,0 +1,22 @@
+klass_a = class A
+  def initialize
+    @a = 11
+    @@a = 22
+    a = 33
+  end
+  @a = 1
+  @@a = 2
+  a = 3
+end
+
+puts A.instance_variable_get "@a"
+puts A.send(:class_variable_get, "@@a")
+puts klass_a
+
+puts A.new.instance_variable_get "@a"
+puts A.send(:class_variable_get, "@@a")
+puts A.new.send(:initialize)
+
+
+
+
